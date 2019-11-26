e4367f7dc8bdf9ff07eaaa52da5cf2d32f535987
Charlie Paterson <Charlieed.paterson@googlemail.com>
2019-11-26 14:20:39 +0000

    Have created a basic website with a cat picture

---
+++ app.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+
+get '/' do
+  "Welcome to the jam!"
+end
+
+# get '/blorp' do
+#   "<div><strong>Change tHE WORLd, </strong></div>
+#   <img style='border:150px solid lime' src='https://cdn.mos.cms.futurecdn.net/QSY3jwZfMgjeo3p8DZBom9-320-80.jpg'>
+#   <div><strong>my. f i n a l m e S S a g e</strong></div>"
+# end
+
+get '/cheezborgar' do
+  erb(:index)
+end
