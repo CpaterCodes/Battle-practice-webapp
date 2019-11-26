cf366d6f73df2686bbc345277f9074087934abe0
Charlie Paterson <Charlieed.paterson@googlemail.com>
2019-11-26 16:36:30 +0000

    Set up form to use post request

--- app.rb
+++ app.rb
@@ -4,18 +4,11 @@ get '/' do
   "Welcome to the jam!"
 end
 
-# get '/blorp' do
-#   "<div><strong>Change tHE WORLd, </strong></div>
-#   <img style='border:150px solid lime' src='https://cdn.mos.cms.futurecdn.net/QSY3jwZfMgjeo3p8DZBom9-320-80.jpg'>
-#   <div><strong>my. f i n a l m e S S a g e</strong></div>"
-# end
-
-get '/random-cheezborgar' do
-  @name = ['Amigo','Oskar','Merovingian'].sample
-  erb(:index)
+get '/cheezborgar-form' do
+  erb(:cat_form)
 end
 
-get '/named-cheezborgar' do
+post '/named-cheezborgar' do
   @name = params[:name]
   erb(:index)
 end
